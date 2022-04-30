b8645b0fa8fa3a7a053d53180b60e9d99a2fa27e
Lareta064 <lareta064@gmail.com>
2022-04-30 17:15:17 +0300

    quizUi

--- src/js/main.js
+++ src/js/main.js
@@ -3,25 +3,66 @@ document.addEventListener("DOMContentLoaded", function (){
 	const navIcon = document.querySelector('.nav-icon');
 	const bodyEl = document.body;
 	const overlay = document.querySelector('#overlay');
-	navIcon.addEventListener('click', function () {
-		if(this.classList.contains('nav-icon--active')){
-			this.classList.remove('nav-icon--active');
-			asideMenu.classList.remove('aside-menu--active');
-			overlay.classList.remove('overlay--active');
-			bodyEl.classList.remove('noscroll');
-		}else{
-			this.classList.add('nav-icon--active');
-			asideMenu.classList.add('aside-menu--active');
-			bodyEl.classList.add('noscroll');
-			overlay.classList.add('overlay--active');
-		}
-		
-
-		
-	});
-
+	if(asideMenu){
+		navIcon.addEventListener('click', function () {
+			if(this.classList.contains('nav-icon--active')){
+				this.classList.remove('nav-icon--active');
+				asideMenu.classList.remove('aside-menu--active');
+				overlay.classList.remove('overlay--active');
+				bodyEl.classList.remove('noscroll');
+			}else{
+				this.classList.add('nav-icon--active');
+				asideMenu.classList.add('aside-menu--active');
+				bodyEl.classList.add('noscroll');
+				overlay.classList.add('overlay--active');
+			}
+			
+		});
+	}
+	/*===========input RANGE========= */
 	
 
+	var sheet = document.createElement('style'),
+    $rangeInput = $('.range input'),
+    prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+
+    $rangeInput.val('1');
+
+    document.body.appendChild(sheet);
+
+    var getTrackStyle = function (el) {
+        var curVal = el.value,
+            val = (curVal - 1) * 20,
+            style = '';
+
+        // Set active label
+        $('.range-labels li').removeClass('active selected');
+
+        var curLabel = $('.range-labels').find('li:nth-child(' + curVal + ')');
+
+        curLabel.addClass('active selected');
+        curLabel.prevAll().addClass('selected');
+
+        // Change background gradient
+        for (var i = 0; i < prefs.length; i++) {
+            style += '.range {background: linear-gradient(to right, #00B886 0%, #00B886 ' + val + '%, #fff ' + val + '%, #fff 100%)}';
+            style += '.range input::-' + prefs[i] + '{background: linear-gradient(to right, #00B886 0%, #00B886 ' + val + '%, #DAE7EB ' + val + '%, #DAE7EB 100%)}';
+        }
+
+        return style;
+    }
+
+    $rangeInput.on('input', function () {
+        sheet.textContent = getTrackStyle(this);
+    });
+
+    $('.range-labels li').on('click', function () {
+        var index = $(this).index();
+
+        $rangeInput.val(index + 1).trigger('input');
+        $(".range-results-wrap").html($(this).html());
+    });
+	/************************************* */
 
 
 })
